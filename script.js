5980b72f81c57863506e763421f6e8a4ba8f8206
jaydespo36-web <jaydespo36@gmail.com>
2026-06-11 14:13:02 +0800

    Initial portfolio upload

--- script.js
+++ script.js
@@ -4,3 +4,98 @@ const navLinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+// LIGHTBOX
+
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close-btn");
+
+galleryImages.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        lightbox.classList.add("show");
+        lightboxImg.src = img.src;
+
+        document.body.style.overflow = "hidden";
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    lightbox.classList.remove("show");
+
+    document.body.style.overflow = "auto";
+});
+
+lightbox.addEventListener("click", (e) => {
+
+    if(e.target === lightbox){
+
+        lightbox.classList.remove("show");
+
+        document.body.style.overflow = "auto";
+    }
+});
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.classList.add("show");
+        lightboxImg.src = img.src;
+        document.body.style.overflow = "hidden";
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if(e.target === lightbox){
+        lightbox.classList.remove("show");
+        document.body.style.overflow = "auto";
+    }
+});
+const typingText = document.querySelector(".typing-text");
+
+const words = ["Irish Mae R. Binarao "];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentWord = words[wordIndex];
+
+    if (isDeleting) {
+        typingText.textContent = currentWord.substring(0, charIndex--);
+    } else {
+        typingText.textContent = currentWord.substring(0, charIndex++);
+    }
+
+    let speed = isDeleting ? 80 : 120;
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        speed = 1500; // pause
+        isDeleting = true;
+    }
+
+    if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        speed = 500;
+    }
+
+    setTimeout(typeEffect, speed);
+}
+
+typeEffect();
